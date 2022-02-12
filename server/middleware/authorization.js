ae48ff994d899d1c8c6dd916340a458e7d9abbe5
eliolias <65453595+eliolias@users.noreply.github.com>
2022-02-12 07:58:27 -0500

    login and registration route creation

---
+++ server/middleware/authorization.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config()
+
+module.exports = async (req, res, next) => {
+    try {
+
+        //1.desctructure and check the token is valid/exists
+        const jwtToken = req.header("token");
+        if (!jtwToken){
+            return res.status(403).json("Not Authorized")
+        }
+        
+        const payload = jwt.verify(jwtToken, process.env.Secret)
+
+        req.user = payload.user;
+
+
+    } catch (err) {
+        console.error(err.message);
+        return res.status(403).json("Not Authorized")
+    }
+};
